5d2a704c238eb9b4bbeea85f30f703c258e54bf1
Pedro Nunes <pedroskzt@gmail.com>
2025-05-22 14:35:07 -0700

    Add Category model and integrate with MenuItem

--- api/menu/menu_models.py
+++ api/menu/menu_models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 
-# class Category(models.Model):
-#     slug = models.SlugField(default="", null=False)
-#     title = models.CharField(max_length=255, db_index=True)
-#
-#     def __str__(self):
-#         return self.title
+class Category(models.Model):
+    slug = models.SlugField(blank=False, unique=True)
+    title = models.CharField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 class MenuItem(models.Model):
@@ -14,7 +20,7 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
     delivery = models.BooleanField(db_index=True)
-    # category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category)
     image = models.ImageField(upload_to='img/menu_items/')
 
     def __str__(self):

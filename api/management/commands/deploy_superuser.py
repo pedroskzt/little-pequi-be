da9a1ed6209c84b714ec0d5f0f8868edf25b265c
Pedro Nunes <pedroskzt@gmail.com>
2025-06-02 11:49:33 -0700

    Update the deploy_superuser command to accept first_name and last_naem.

--- api/management/commands/deploy_superuser.py
+++ api/management/commands/deploy_superuser.py
@@ -1,6 +1,7 @@
 import os
-from django.core.management.base import BaseCommand
+
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
@@ -17,7 +18,8 @@ class Command(BaseCommand):
         Execute the command to create a superuser.
         Required environment variables:
         - DJANGO_SUPERUSER_EMAIL
-        - DJANGO_SUPERUSER_USERNAME
+        - DJANGO_SUPERUSER_FIRST_NAME
+        - DJANGO_SUPERUSER_LAST_NAME
 
         Optional environment variables:
         - DJANGO_SUPERUSER_PASSWORD
@@ -32,7 +34,8 @@ class Command(BaseCommand):
         if not User.objects.filter(is_superuser=True).exists():
             # Get superuser credentials from environment variables
             email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
-            username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+            first_name = os.environ.get('DJANGO_SUPERUSER_FIRST_NAME')
+            last_name = os.environ.get('DJANGO_SUPERUSER_LAST_NAME')
             password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
             # Generate a random password if none is provided in environment variables
@@ -41,7 +44,7 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.WARNING(f"Random password generated: {password}"))
 
             # Create the superuser with the email as both username and email
-            User.objects.create_superuser(username=username, email=email, password=password)
+            User.objects.create_superuser(first_name=first_name, last_name=last_name, email=email, password=password)
             self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
         else:
             # Inform that a superuser already exists

bd1dcee6b51d28cc0ea894e743221f0832e246c8
Pedro Nunes <pedroskzt@gmail.com>
2025-06-26 18:42:30 -0700

    Use settings for menu item image upload path.

--- backend/settings.py
+++ backend/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
+MENU_ITEM_MEDIA_ROOT = 'menu_items/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

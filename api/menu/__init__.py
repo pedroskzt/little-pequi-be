c1744d1427c27d5d1a371a1a8578519193c1a9a7
Pedro Nunes <pedroskzt@gmail.com>
2025-12-03 14:20:39 -0800

    Update references.

--- api/menu/__init__.py
+++ api/menu/__init__.py
@@ -1,3 +1,3 @@
-from .menu_models import (MenuItem, Category)
-from .menu_serializers import (MenuItemSerializer, CategorySerializer)
-from .menu_views import (MenuItemViewSet, CategoryViewSet)
+from .menu_models import (MenuItem, Tag, Category)
+from .menu_serializers import (MenuItemSerializer, TagSerializer, CategorySerializer, MenuItemImageSerializer)
+from .menu_views import (MenuItemViewSet, TagViewSet, CategoryViewSet)

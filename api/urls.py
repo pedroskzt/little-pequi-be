fb8f0af2b71ccefa014f1ea23e89c1a4b436c101
Pedro Nunes <pedroskzt@gmail.com>
2025-12-03 14:23:09 -0800

    Add URL for Tag APIs

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import SimpleRouter
 
-from api.menu import MenuItemViewSet, CategoryViewSet
+from api.menu import MenuItemViewSet, TagViewSet, CategoryViewSet
 
 router = SimpleRouter(trailing_slash=False)
+router.register('tag', TagViewSet, basename='tag')
 router.register('category', CategoryViewSet, basename='category')
 router.register('menu-item', MenuItemViewSet, basename='menu-item')
 

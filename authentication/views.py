58831ea915c39625e7e59e2d58ad9e4d66efc66f
Pedro Nunes <pedroskzt@gmail.com>
2026-01-08 14:09:14 -0800

    Update sign-in api to use HTTP Only cookies for JWT tokens

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,5 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
@@ -5,7 +7,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 class LoginViewSet(TokenObtainPairView):
-    pass
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        response = super().post(request, *args, **kwargs)
+        tokens = response.data
+        response.data = {}
+        response.set_cookie('access', tokens['access'], httponly=True, secure=True, samesite='Strict')
+        response.set_cookie('refresh', tokens['refresh'], httponly=True, secure=True, samesite='Strict')
+        print(response.data)
+        return response
 
 
 class RefreshViewSet(TokenRefreshView):
